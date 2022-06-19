5928ffa390e3cb41558631e8b1239174d1286fe7
Dzmitry <forgingfutureself@gmail.com>
2022-06-19 22:51:03 +0300

    homework2: task 4 correction

--- homework2/src/com/prokopchik/homework2/homeworkTask4.java
+++ homework2/src/com/prokopchik/homework2/homeworkTask4.java
@@ -5,17 +5,17 @@ public class homeworkTask4 {
     /*Определить число, полученное выписыванием в обратном порядке цифр
 любого 4-х значного натурального числа n.*/
     public static void main(String[] args){
-        int n = 5432;
+//        int n = 5432;
+        int n = 1010;
 
         int x4 = n / 1000;
         int x3 = (n - x4 * 1000) / 100;
         int x2 = (n - x4 * 1000 - x3 * 100) / 10;
         int x1 = n % 10;
 
-        System.out.print(x1);
-        System.out.print(x2);
-        System.out.print(x3);
-        System.out.print(x4);
+        int newInt = (x1 * 1000) + (x2 * 100) + (x3 * 10) + x4;
+
+        System.out.println(newInt);
 
     }
 }
