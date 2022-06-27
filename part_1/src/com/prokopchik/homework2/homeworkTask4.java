0620ea4fe6f5b1167036aba1282d6a4128452896
Dzmitry <forgingfutureself@gmail.com>
2022-06-27 08:37:29 +0300

    homework 2: task 3, 4, 5 added. Another try to create everything properly

---
+++ part_1/src/com/prokopchik/homework2/homeworkTask4.java
@@ -0,0 +1,22 @@
+package com.prokopchik.homework2;
+
+import java.util.Scanner;
+/*Определить число, полученное выписыванием в обратном порядке цифр
+любого 4-х значного натурального числа n.*/
+public class homeworkTask4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите 4-хзначное наиуральное чмсло");
+        int n = scanner.nextInt();
+
+        int x4 = n / 1000;
+        int x3 = (n - x4 * 1000) / 100;
+        int x2 = (n - x4 * 1000 - x3 * 100) / 10;
+        int x1 = n % 10;
+
+        int newInt = (x1 * 1000) + (x2 * 100) + (x3 * 10) + x4;
+
+        System.out.println(newInt);
+
+    }
+}
